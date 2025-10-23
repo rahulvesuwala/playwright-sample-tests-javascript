49f799b549142b768899f0aa485dc25a17b680f2
rahul <rahul.alphabin@gamil.com>
2025-10-23 11:10:47 +0530

    Testing

--- tests/visual.spec.js
+++ tests/visual.spec.js
@@ -11,10 +11,10 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/');
 });
 
-test('test @chromium', async ({ page }) => {
-  await page.goto('https://github.com/login');
-  await expect(page).toHaveScreenshot('github-login.png');
-  await page.getByRole('textbox', { name: 'Username or email address' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).fill('test');
-  await expect(page).toHaveScreenshot('github-login.png');
-});
+// test('test @chromium', async ({ page }) => {
+//   await page.goto('https://github.com/login');
+//   await expect(page).toHaveScreenshot('github-login.png');
+//   await page.getByRole('textbox', { name: 'Username or email address' }).click();
+//   await page.getByRole('textbox', { name: 'Username or email address' }).fill('test');
+//   await expect(page).toHaveScreenshot('github-login.png');
+// });
